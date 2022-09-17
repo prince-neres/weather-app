2d5c704a4988c2d61f0ebc847cb2647c6e0f529d
Prince Neres <prince.neres@ambar.tech>
2022-09-17 11:47:52 -0300

    Ajustes iniciais

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Axios from 'axios'
+import CityComponent from './components/CityComponent'
+import WeatherComponent from './components/WeatherInfoComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const WeatherIcons = {
+  "01d": "/imgs/sunny.png",
+  "01n": "/imgs/night.png",
+  "02d": "/imgs/day.png",
+  "02n": "/imgs/cloudy-night.png",
+  "03d": "/imgs/cloudy.png",
+  "03n": "/imgs/cloudy.png",
+  "04d": "/imgs/perfect-day.png",
+  "04n": "/imgs/cloudy-night.png",
+  "09d": "/imgs/rain.png",
+  "09n": "/imgs/rain-night.png",
+  "10d": "/imgs/rain.png",
+  "10n": "/imgs/rain-night.png",
+  "11d": "/imgs/storm.png",
+  "11n": "/imgs/storm.png",
 }
 
-export default App;
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 20px;
+  margin: auto;
+  color: white;
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, .2); 
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, .15);
+  backdrop-filter: blur(5px);
+`
+
+const Title = styled.span`
+  color: white;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+function App() {
+  const [city, updateCity] = useState()
+  const [weather, updateWeather] = useState()
+  const fetchWeather = async (e) => {
+    e.preventDefault()
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPEN_WHEATHER_KEY}`,
+    )
+    updateWeather(response.data)
+  }
+  return (
+    <Card>
+      <Title>Weather App</Title>
+      {city && weather ? (
+        <WeatherComponent weather={weather} city={city} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Card>
+  )
+}
+
+export default App
---
+++ src/components/CityComponent.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+import React from 'react'
+import { WeatherIcons } from '../App'
+
+const SearchBox = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin: 20px;
+  border: none;
+  border-radius: 5px;
+  & input {
+    padding: 10px;
+    font-size: 14px;
+    border: none;
+    outline: none;
+    font-weight: bold;
+  }
+  & button {
+    background-color: white;
+    font-size: 14px;
+    padding: 0 10px;
+    color: black;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-weight: bold;
+  }
+`
+
+const ChooseCityLabel = styled.span`
+  color: white;
+  margin: 10px auto;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const WelcomeWeatherLogo = styled.img`
+  width: 250px;
+  height: 200px;
+  margin: 40px auto;
+`
+
+const CityComponent = (props) => {
+  const { updateCity, fetchWeather } = props
+
+  return (
+    <>
+      <WelcomeWeatherLogo src={WeatherIcons['04d']}/>
+      <ChooseCityLabel>Find Weather of your city</ChooseCityLabel>
+      <SearchBox onSubmit={fetchWeather}>
+        <input
+          onChange={(e) => updateCity(e.target.value)}
+          placeholder='City'
+        />
+        <button type={'submit'}>Search</button>
+      </SearchBox>
+    </>
+  )
+}
+
+export default CityComponent
